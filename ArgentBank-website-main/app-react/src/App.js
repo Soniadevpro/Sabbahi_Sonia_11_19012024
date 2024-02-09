29bef6a1d4ee84e9f5c9bb13ca98654acd4dc330
Sonia Sabbahi <soniadevpro@gmail.com>
2024-02-09 13:50:24 +0100

    install project

--- ArgentBank-website-main/app-react/src/App.js
+++ ArgentBank-website-main/app-react/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+import Home from "./Views/Home/Home";
+import SignIn from "./Views/SignIn/SignIn";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Error from "./Views/Error/Error";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />;
+        <Route path="/signin" element={<SignIn />} />;
+        <Route path="*" element={<Error />} />;
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
