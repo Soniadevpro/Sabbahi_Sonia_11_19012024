3715ad89ab35e6b621e62d8e7b117df8584dfeb8
Sonia Sabbahi <soniadevpro@gmail.com>
2024-02-13 15:13:32 +0100

    update refacto

--- ArgentBank-website-main/app-react/src/Components/Footer/Footer.js
+++ ArgentBank-website-main/app-react/src/Components/Footer/Footer.js
@@ -4,9 +4,8 @@ import "../Footer/Footer.css";
 const Footer = () => {
   return (
     <div>
-      {" "}
-      <footer class="footer">
-        <p class="footer-text">Copyright 2020 Argent Bank</p>
+      <footer className="footer">
+        <p className="footer-text">Copyright © 2020 Argent Bank</p>
       </footer>
     </div>
   );
